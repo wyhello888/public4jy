f24d9ea37af4954b5d32b7ce033dd72eaf692505
zhaochengxue <zh_chengxue@163.com>
2018-01-04 13:10:09 +0800

    ppt ,大纲，教学手册，案例库

---
+++ samples/H5_p2_samples_20180104_zcx/day01/js/demo1.js
@@ -0,0 +1,11 @@
+//在页页上打印出一条信息！document.write("");
+document.write("hello javaScript!<br/>");
+document.write("<h1>你好，js!</h1>");
+
+//console.log() 在控制台输出信息
+console.log("hello javascript!");
+console.log("你好，js!");
+
+//window.alert() 在页面上弹出一个窗口
+window.alert("hello JavaScript!");
+alert("您好！js");
